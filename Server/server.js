7b46432ea998680d144abf3ae3e9446fbf1da9a7
Sindhu P <sindhupvly@gmail.com>
2025-08-10 19:19:30 +0530

    Update server.js

--- Server/server.js
+++ Server/server.js
@@ -21,7 +21,7 @@ await connectDB()
 // await connectCloudinary()
 
 //Allow multiple origins
-const allowedOrigins = ['http://localhost:5173']
+const allowedOrigins = ['http://localhost:5173' , 'https://greencart-project-frontend.vercel.app']
 
 app.post('/stripe', express.raw({type: 'application/json'}), stripeWebhooks)
 
